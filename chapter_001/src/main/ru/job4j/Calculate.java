a8b35719d9249d150af43621b3ef4758994a5449
PavelZubaha <Apximar@gmail.com>
2017-03-05 13:02:18 +0200

    task 4087

--- chapter_001/src/main/ru/job4j/Calculate.java
+++ chapter_001/src/main/ru/job4j/Calculate.java
@@ -1,7 +1,16 @@
 package ru.job4j;
-
-public class Calculate{
-	public static void main(String[] args){
+/**
+ *Class contents solution of task 4087
+ *@author pzubaha
+ *@since 05.03.2017
+ *@version 1
+*/
+public class Calculate {
+	/**
+	*Constructor output String to console
+	*@param args args
+	*/
+	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
 }
