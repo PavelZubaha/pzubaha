78383db002c32275d4041ebc48fc00a5ffac65ac
PavelZubaha <Apximar@gmail.com>
2017-03-12 22:17:20 +0200

    task 185 added test for div,multiple

--- chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
+++ chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
@@ -23,7 +23,7 @@ public class CalculatorTest {
 		assertThat(result, is(expected));
 	}
 	/**
-	*Test add.
+	*Test substruct.
 	*/
 	@Test
 	public void whenSubstructTwoMinusOneThenOne() {
@@ -31,6 +31,28 @@ public class CalculatorTest {
 		calc.substruct(2D, 1D);
 		double result = calc.getResult();
 		double expected = 1D;
+		assertThat(result, is(expected));
+	}
+	/**
+	*Test div.
+	*/
+	@Test
+	public void whenDivFourDivTwoThenTwo() {
+		Calculator calc = new Calculator();
+		calc.div(4D, 2D);
+		double result = calc.getResult();
+		double expected = 2D;
+		assertThat(result, is(expected));
+	}
+	/**
+	*Test multiple.
+	*/
+	@Test
+	public void whenMultipleTwoMultiplyTwoThenFour() {
+		Calculator calc = new Calculator();
+		calc.multiple(2D, 2D);
+		double result = calc.getResult();
+		double expected = 4D;
 		assertThat(result, is(expected));
 	}
 }
