cf0f8a4bdfca14273ece48c7bb4a78ab01af4f7a
PavelZubaha <Apximar@gmail.com>
2017-03-19 14:49:55 +0200

    task 225

---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.array;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+import org.junit.Test;
+
+/**
+ * Part 1. Base syntax.
+ * Lesson 5. Test removing duplicates in array.
+ * Task 5.3. 225 a program for removing duplicates from array of String
+ *
+ * @author Zubaha Pavel
+ * @version 1
+ * @since 19.03.2017
+*/
+public class ArrayDuplicateTest {
+	/**
+	*Test case when array has duplicates.
+	*/
+	@Test
+	public void whenRemoveDuplicatesThenArrayWithoutDuplicate() {
+		ArrayDuplicate arrayDuplicate = new ArrayDuplicate();
+		String[] originalStrings = {"Hello, ", "Hello, ", "World", "!", "!", "!", "World", "!", "!", "World", "Hello, "};
+		String[] expected = {"Hello, ", "World", "!"};
+		assertThat(arrayDuplicate.remove(originalStrings), is(expected));
+	}
+}
