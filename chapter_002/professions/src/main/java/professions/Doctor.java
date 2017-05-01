4e563d063cb53d9f483c1d8de1b3d8d44af94de5
PavelZubaha <Apximar@gmail.com>
2017-05-01 20:59:47 +0300

    change project structure(profession)

---
+++ chapter_002/professions/src/main/java/professions/Doctor.java
@@ -0,0 +1,137 @@
+package professions;
+
+/**
+ * Chapter 2. OOP.
+ * Lesson 3. Inheritance.
+ * Representation of doctor in the code.
+ * Class contains solution of task 6837.
+ * @author Pavel Zubaha
+ * @since 1
+ * @version 1
+*/
+public class Doctor extends Profession {
+	/**
+	 * specialization of doctor.
+	*/
+	private String specialization;
+	/**
+	 * certificate of doctor.
+	*/
+	private String certificate;
+	/**
+	 * current patient index.
+	*/
+	private int currentPatientIndex = 0;
+	/**
+	 * doctor's signature.
+	*/
+	private final String doctorSignature = "DOCTOR_SIGNATURE";
+	/**
+	 * constructor of Doctor.
+	 * @param name - doctor's name,
+	 * @param age - set doctor age,
+	 * @param experience - set doctor experience,
+	 * @param location - doctor location,
+	 * @param specialization - doctor specialization,
+	 * @param certificate - doctor's certificate.
+	*/
+	public Doctor(String name, int age, int experience, String location, String specialization, String certificate) {
+		super(name, age, experience, location);
+		this.specialization = specialization;
+		this.certificate = certificate;
+	}
+	/**
+	 * constructor of Doctor.
+	 * @param specialization - doctor specialization;
+	 * @param certificate - doctor's certificate.
+	*/
+	public Doctor(String specialization, String certificate) {
+		super();
+		this.specialization = specialization;
+		this.certificate = certificate;
+	}
+	/**
+	 * Default constructor of Doctor.
+	*/
+	public Doctor() {
+		this("unknown", "unknown");
+	}
+	/**
+	 * Set doctor's certificate.
+	 * @param certificate - doctor certificate.
+	*/
+	public void setCertificate(String certificate) {
+		this.certificate = certificate;
+	}
+	/**
+	 * Get doctor's certificate.
+	 * @return certificate - doctor certificate.
+	*/
+	public String getDoctorCertificate() {
+		return this.certificate;
+	}
+	/**
+	 * Doctor accept a patient.
+	 * @param patient - current patient.
+	*/
+	public void acceptsPatient(Patient patient) {
+			System.out.println("Please come in!");
+			currentPatientIndex += 1;
+			patient.setPatientsDoctor(this);
+	}
+	/**
+	 * Find diagnose.
+	 * @param patient - current patient.
+	 * @return diagnosis of patient.
+	*/
+	public String diagnose(Patient patient) {
+		String diagnosis = "desease";
+		return diagnosis;
+	}
+	/**
+	 * Write journal.
+	 * @param patient - current patient.
+	 * @return string for writing into journal.
+	*/
+	public String writeJournal(Patient patient) {
+		String separator = System.getProperty("line.separator");
+		return String.join(separator, Integer.toString(currentPatientIndex), patient.getName(), Integer.toString(patient.getAge()), patient.getDiagnosis(), this.doctorSignature);
+	}
+	/**
+	 * Prescribed treatment for patient.
+	 * @param patient - current patient.
+	 * @return string recipe.
+	*/
+	public String recipe(Patient patient) {
+		return "recipe";
+	}
+	/**
+	 * Write Certificate of illness for patient.
+	 * @param patient - current patient.
+	 * @return string for writing sickness certificate.
+	*/
+	public String writePatientSicknessCertificate(Patient patient) {
+		String separator = System.getProperty("line.separator");
+		return String.join(separator,
+			"Certificate of Sickness",
+			"Number: " + Integer.toString(this.currentPatientIndex),
+			"patient name: " + patient.getName(),
+			"Diagnosis: " + patient.getDiagnosis(),
+			"Doctor name: " + this.getName(),
+			"Signature: " + this.doctorSignature);
+	}
+	/**
+	 * Get current amount of doctor's patients.
+	 * @return current patient amount.
+	*/
+	public int getPatientIndex() {
+		 return this.currentPatientIndex;
+	}
+	/**
+	 * Get doctor's signature.
+	 * @return doctor signature.
+	*/
+	public String getDoctorSignature() {
+		return this.doctorSignature;
+	}
+}
---
+++ chapter_002/professions/src/main/java/professions/Patient.java
@@ -0,0 +1,106 @@
+package professions;
+
+/**
+ * Chapter 2. OOP.
+ * Lesson 3. Inheritance.
+ * Representation of patient in the code.
+ * Class contains solution of task 6837.
+ * @author Pavel Zubaha
+ * @since 24.04.2017
+ * @version 1
+*/
+public class Patient extends Person {
+	/**
+	 * symptoms of patient.
+	*/
+	private String[] symptoms;
+	/**
+	 * current diagnosis of patient.
+	*/
+	private String diagnosis;
+	/**
+	 * current patient's doctor.
+	*/
+	private Doctor patientsDoctor;
+	/**
+	 * current patient's doctor.
+	*/
+	private String sicknessCertificate;
+	/**
+	 * Default constructor of patient.
+	*/
+	public Patient() {
+		this("unnamed");
+	}
+	/**
+	 * Constructor of patient.
+	 * @param name - name of patient.
+	*/
+	public Patient(String name) {
+		this(name, 0);
+	}
+	/**
+	 * Constructor of patient.
+	 * @param name - patient name.
+	 * @param age - age of patient.
+	*/
+	public Patient(String name, int age) {
+		super(name, age);
+		this.diagnosis = "unknown";
+		this.patientsDoctor = new Doctor();
+	}
+	/**
+	 * Method for seting patient symptoms.
+	 * @param symptoms - symptoms of patient.
+	*/
+	public void setSymptoms(String[] symptoms) {
+		this.symptoms = symptoms;
+	}
+	/**
+	 * Method get patient's symptoms.
+	 * @return patient symptoms.
+	*/
+	public String[] getSymptoms() {
+		return this.symptoms;
+	}
+	/**
+	 * Method for set patient's doctor.
+	 * @param patientsDoctor - doctor that accepts patient.
+	*/
+	public void setPatientsDoctor(Doctor patientsDoctor) {
+		this.patientsDoctor = patientsDoctor;
+	}
+	/**
+	 * Method for get patient's doctor.
+	 * @return doctor of patient.
+	*/
+	public Doctor getPatientsDoctor() {
+		return this.patientsDoctor;
+	}
+	/**
+	 * Method for set patient's sickness certificate.
+	*/
+	public void setSicknessCertificate() {
+		this.sicknessCertificate = this.patientsDoctor.writePatientSicknessCertificate(this);
+	}
+	/**
+	 * Method for get patient's sickness certificate.
+	 * @return - sickness certificate of patient.
+	*/
+	public String getSicknessCertificate() {
+		return this.sicknessCertificate;
+	}
+	/**
+	 * Method for set patient's diagnosis.
+	*/
+	public void setDiagnosis() {
+		this.diagnosis = this.patientsDoctor.diagnose(this);
+	}
+	/**
+	 * Method for get patient's diagnosis.
+	 * @return diagnosis of patient.
+	*/
+	public String getDiagnosis() {
+		return this.diagnosis;
+	}
+}
